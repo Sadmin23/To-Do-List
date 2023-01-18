c6770effde9ab6cefd094ab02ae925944bf2ff87
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-18 20:50:41 +0600

    day no. generated

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay == 6 || currentDay == 0){
+        res.sendFile(__dirname + "/weekend.html");
+    }
+    else{
+        res.sendFile(__dirname + "/weekday.html");
+    }
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
