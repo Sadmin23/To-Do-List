f653a4f63e25aed01ba8c0c7c705aaa7a0a9ccf3
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-19 11:36:28 +0600

    item add feature

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var item="";
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day="";
 
-    switch (currentDay) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-           day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
-      }
+    var options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+    };
 
-    res.render("list", {Dday: day});
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {Dday: day, newListitem: item});
 })
+
+app.post("/", function(req, res){
+  
+  item = req.body.newItem;
+
+  res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
