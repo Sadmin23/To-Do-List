2384d0d3b9dcf848e58a8c90de9e24c4f8e18fb9
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-10 21:38:20 +0600

    delete items added

--- app.js
+++ app.js
@@ -58,6 +58,18 @@ app.post("/", function(req, res){
   res.redirect("/");
 });
 
+app.post("/delete", function(req, res){
+
+  const checkedItemId = req.body.checkbox;
+
+  item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err){
+      console.log("Successfully deleted the item!");
+      res.redirect("/");
+    }
+  });
+});
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
